94b8a55838a56401c06eee177507d336c612286d
Anderson Clayton Ribas <andersex03@gmail.com>
2023-11-13 19:28:41 -0300

    Add files via upload

---
+++ funcio.js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('cadastroFamiliaBtn').addEventListener('click', function() {
+    window.location.href = 'cadastroFamilia.html';
+});
+
+document.getElementById('cadastroDoadorBtn').addEventListener('click', function() {
+    window.location.href = 'cadastroDoador.html';
+});
