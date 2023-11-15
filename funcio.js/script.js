c94fd3438f8c55a00611d020075213b55df5e93e
Anderson Clayton Ribas <andersex03@gmail.com>
2023-11-15 17:36:35 -0300

    Add files via upload

--- funcio.js/script.js
+++ funcio.js/script.js
@@ -5,3 +5,15 @@ document.getElementById('cadastroFamiliaBtn').addEventListener('click', function
 document.getElementById('cadastroDoadorBtn').addEventListener('click', function() {
     window.location.href = 'cadastroDoador.html';
 });
+function submitForm() {
+    var doador = document.getElementById('doador').value;
+    var alimento = document.getElementById('alimento').value;
+    var quantidade = document.getElementById('quantidade').value;
+
+    // Você pode fazer algo com os dados, como enviar para um servidor ou exibir em um console.
+    console.log('Doador:', doador);
+    console.log('Tipo de Alimento:', alimento);
+    console.log('Quantidade:', quantidade);
+
+    // Adicione aqui a lógica para enviar os dados para o servidor, se necessário.
+}
